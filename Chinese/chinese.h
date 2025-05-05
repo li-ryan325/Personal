def7003c2c1bd43cd004756b151438f6bc65847a
Ryan Li <li.ryan158g@gmail.com>
2025-05-05 14:56:25 -0700

    Added Spring 2025

---
+++ Chinese/chinese.h
@@ -0,0 +1,8 @@
+#define CHINESE_H_
+#ifndef CHINESE_H_
+
+class Study {
+
+};
+
+#endif //CHINESE_H_
---
+++ Chinese/main.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <chinese.h>
+
+int main(int argc, char* argv[]) {
+    std::cout << "hello";
+    return 0;
+}
